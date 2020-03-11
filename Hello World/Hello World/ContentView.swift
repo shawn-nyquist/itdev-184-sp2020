d9478efa05b65c0be04bf1ed3e731d4e8718fe90
Shawn Nyquist <user162490@TX013.local>
2020-03-11 18:14:11 -0500

    Activity-2

---
+++ Hello World/Hello World/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hello World
+//
+//  Created by user162490 on 3/11/20.
+//  Copyright © 2020 ShawnNyquist. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
